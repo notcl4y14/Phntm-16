831a0145f5ebcb8b308e0feeb787097fb2685d72
notcl4y14 <notcl4y@gmail.com>
2024-09-01 17:07:50 +0300

    Make Cartridge run JavaScript code

--- common/cartridge.js
+++ common/cartridge.js
@@ -1,9 +1,34 @@
+import Display from "../core/display.js";
+import { Context } from "./context.js";
+
 export default class Cartridge {
 
 	code;
 
 	constructor(code) {
 		this.code = code;
+	}
+
+	run () {
+		const func = new Function("context", this.code + "return { update, draw }");
+		return func( Context );
+		// func(
+		// 	(color) => {
+		// 		Display.context.fillStyle = color;
+		// 	},
+		// 	(x, y, width, height) => {
+		// 		Display.context.rect(x, y, width, height);
+		// 	},
+		// 	() => {
+		// 		Display.context.fill();
+		// 	}
+		// );
+		// console.log("Start running");
+
+		// const vm = new VirtualMachine(this.code);
+		// vm.run();
+
+		// console.log("Program finished!");
 	}
 	
 }
